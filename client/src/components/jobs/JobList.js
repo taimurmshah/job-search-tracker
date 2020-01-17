5e9d5a9de0fe36ca1f61e01120cc49be3d98b4f9
github username <taimurmshah@gmail.com>
2020-01-17 15:39:48 -0500

    filter working

--- client/src/components/jobs/JobList.js
+++ client/src/components/jobs/JobList.js
@@ -1,24 +1,107 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
 import { Menu, Span } from "../resusable-components/styledComponents";
 import { connect } from "react-redux";
 import { currentJob } from "../../redux/actions/job";
 import { getEmployeesThunk } from "../../redux/thunks/employee";
 import JobCard from "./JobCard";
+import Checkbox from "../resusable-components/Checkbox";
 
-const JobList = ({ jobs }) => {
-  jobs = jobs.map(job => (
-    <JobCard
-      key={job._id}
-      _id={job._id}
-      company={job.company}
-      status={job.status}
-      date={job.mostRecentEmailSent}
-    />
-  ));
+class JobList extends Component {
+  state = {
+    followUp: false,
+    active: false
+  };
 
-  return <Menu>{jobs}</Menu>;
-};
+  followUpHandler = () => {
+    this.setState({
+      followUp: !this.state.followUp,
+      active: false
+    });
+  };
+
+  activeHandler = () => {
+    this.setState({
+      followUp: false,
+      active: !this.state.active
+    });
+  };
+
+  render() {
+    const jobs = this.props.jobs.map(job => (
+      <JobCard
+        key={job._id}
+        _id={job._id}
+        company={job.company}
+        status={job.status}
+        date={job.mostRecentEmailSent}
+      />
+    ));
+
+    const today = new Date();
+
+    let followUpJobs = this.props.jobs
+      .filter(j => j.mostRecentEmailSent !== null)
+      .filter(j => {
+        const lastEmail = new Date(j.mostRecentEmailSent);
+        const diffTime = Math.abs(today - lastEmail);
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return diffDays > 7;
+      })
+      .filter(
+        j =>
+          j.status === "Waiting for email response" ||
+          j.status === "Submitted application; waiting for company response"
+      )
+      .map(j => (
+        <JobCard
+          key={j._id}
+          _id={j._id}
+          company={j.company}
+          status={j.status}
+          date={j.mostRecentEmailSent}
+        />
+      ));
+
+    const activeJobs = this.props.jobs
+      .filter(j => j.status !== "Rejected")
+      .map(j => (
+        <JobCard
+          key={j._id}
+          _id={j._id}
+          company={j.company}
+          status={j.status}
+          date={j.mostRecentEmailSent}
+        />
+      ));
+
+    console.log("jobs length:", jobs.length);
+    console.log("follow up length:", followUpJobs.length);
+    console.log("active length:", activeJobs.length);
+
+    return (
+      <Menu>
+        {!this.state.active && (
+          <Checkbox
+            text={"Follow Up?"}
+            clickHandler={this.followUpHandler}
+            checked={this.state.followUp}
+          />
+        )}
+        {!this.state.followUp && (
+          <Checkbox
+            text={"Active Jobs"}
+            clickHandler={this.activeHandler}
+            checked={this.state.active}
+          />
+        )}
+        {/*{!this.state.followUp ? jobs : followUpJobs}*/}
+        {this.state.active === false && this.state.followUp === false && jobs}
+        {this.state.followUp && followUpJobs}
+        {this.state.active && activeJobs}
+      </Menu>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
