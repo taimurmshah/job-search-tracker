3c178ced1635c8cd8146812e4d05b887a0fa16a4
github username <taimurmshah@gmail.com>
2020-02-19 19:26:17 -0500

    working on css

--- client/src/components/jobs/JobCard.js
+++ client/src/components/jobs/JobCard.js
@@ -18,27 +18,34 @@ const JobCard = ({
   return (
     <>
       <Card>
-        <Span>
-          <Link
-            className="nav-link"
-            onClick={() => {
-              currentJob(_id);
-              getEmployeesThunk(_id);
-            }}
-            to={`/jobs/${_id}`}
-          >
-            {company}
-          </Link>
-        </Span>
-        <Span>
-          <StatusContainer>
-            <P>Status:</P>
-            <P>{status}</P>
-          </StatusContainer>
-        </Span>
-        <Span>
-          <Moment fromNow date={date} />
-        </Span>
+        <Header>
+          <Span>
+            <Link
+              className="nav-link-white"
+              onClick={() => {
+                currentJob(_id);
+                getEmployeesThunk(_id);
+              }}
+              to={`/jobs/${_id}`}
+            >
+              {company}
+            </Link>
+          </Span>
+        </Header>
+        <Body>
+          <Span>
+            <StatusContainer>
+              <P>Status:</P>
+              <P>{status}</P>
+            </StatusContainer>
+          </Span>
+
+          <DaysContainer>
+            <Span>
+              <Moment fromNow date={date} />
+            </Span>
+          </DaysContainer>
+        </Body>
       </Card>
     </>
   );
@@ -56,20 +63,45 @@ export default connect(
   mapDispatchToProps
 )(JobCard);
 
+const Card = styled.div`
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  height: 180px;
+  margin: 10px;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  background: #3a3b41;
+  height: 31px;
+  padding-top: 14px;
+  border-bottom: 5px solid #c8df96;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+}
+`;
+
+const Body = styled.div`
+  width: 100%;
+  background: #d3d3d3;
+  height: 150px;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+`;
+
 const StatusContainer = styled.div`
   display: flex;
   flex-direction: column;
+  padding-left: 15px;
+`;
+
+const DaysContainer = styled.div`
+  position: relative;
+  top: 33px;
 `;
 
 const P = styled.p`
-  font-size: 15px;
+  font-size: 14px;
   padding: 3px 0;
-`;
-
-const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  border: 1px black solid;
-  height: 180px;
-  margin: 10px;
 `;
