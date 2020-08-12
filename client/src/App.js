b854ba8df4a1a06788b306e629796935137e515d
github username <taimurmshah@gmail.com>
2020-08-12 18:58:16 -0400

    app js hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,47 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import { jwtThunk } from "./redux/thunks/auth";
 import { readJobsThunk } from "./redux/thunks/job";
 import PrivateRoute from "./components/auth/PrivateRoute";
-
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
-
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
-
 import Dashboard from "./components/dashboard/Dashboard";
-
 import Jobs from "./components/jobs/Jobs";
 import Job from "./components/jobs/JobContainer";
 import CreateJob from "./components/jobs/CreateJob";
 import Resume from "./components/resume/ViewResume";
 
-class App extends Component {
-  componentDidMount() {
+const App = ({ jwtThunk, readJobsThunk }) => {
+  useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      this.props.jwtThunk(token);
-
-      //todo should i put this here? or should i put it somewhere else? is this expensive?
-      this.props.readJobsThunk();
+      jwtThunk(token);
+      readJobsThunk();
     }
-  }
+  });
 
-  render() {
-    return (
-      <div>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            <PrivateRoute exact path="/jobs" component={Jobs} />
-            <PrivateRoute exact path="/jobs/:id" component={Job} />
-            <PrivateRoute exact path="/create-job" component={CreateJob} />
-            <PrivateRoute exact path="/resume" component={Resume} />
-            <Route exact path="/" component={Landing} />
-          </Switch>
-        </>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <PrivateRoute exact path="/jobs" component={Jobs} />
+          <PrivateRoute exact path="/jobs/:id" component={Job} />
+          <PrivateRoute exact path="/create-job" component={CreateJob} />
+          <PrivateRoute exact path="/resume" component={Resume} />
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </>
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => {
   return {
