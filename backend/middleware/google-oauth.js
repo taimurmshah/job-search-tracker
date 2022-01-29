546bbb0e0488da3f3b836e9acb98f0d4b2ef498f
github username <taimurmshah@gmail.com>
2022-01-29 15:48:54 -0500

    starting up again

--- backend/middleware/google-oauth.js
+++ backend/middleware/google-oauth.js
@@ -5,36 +5,8 @@ const OAuth2 = google.auth.OAuth2;
 const fetch = require("node-fetch");
 const {
   getTokens,
-  fetchUserProfile
+  fetchUserProfile,
 } = require("../helper-methods/google-oauth-helpers");
-
-// const getTokens = async code => {
-//   const oauth2Client = new OAuth2(
-//     process.env.OAUTH_CLIENT,
-//     process.env.OAUTH_SECRET,
-//     process.env.REDIRECT_URI
-//     // "postmessage"
-//   );
-//
-//   const { tokens } = await oauth2Client.getToken(code);
-//   return tokens;
-// };
-
-// const fetchUserProfile = async access_token => {
-//   try {
-//     let res = await fetch("https://openidconnect.googleapis.com/v1/userinfo", {
-//       method: "GET",
-//       headers: {
-//         Authorization: `Bearer ${access_token}`
-//       }
-//     });
-//
-//     res = await res.json();
-//     return res;
-//   } catch (err) {
-//     console.log({ err });
-//   }
-// };
 
 const registerUser = async (userProfile, refresh_token, access_token) => {
   try {
@@ -45,7 +17,7 @@ const registerUser = async (userProfile, refresh_token, access_token) => {
       "google.email": userProfile.email,
       "google.refresh_token": refresh_token,
       "google.access_token": access_token,
-      imageUrl: userProfile.picture
+      imageUrl: userProfile.picture,
     });
     await user.save();
 
@@ -71,7 +43,7 @@ const googleOAuth = async (req, res, next) => {
     //check for existing User
     const existingUser = await User.findOne({
       method: "google",
-      "google.id": googleId
+      "google.id": googleId,
     });
 
     if (tokens.refresh_token) refresh_token = tokens.refresh_token;
@@ -89,10 +61,7 @@ const googleOAuth = async (req, res, next) => {
     existingUser.google.access_token = access_token;
 
     if (refresh_token) {
-      console.log("setting up a new refresh token for the existing user.");
-      console.log("before:", existingUser.google.refresh_token);
       existingUser.google.refresh_token = refresh_token;
-      console.log("after:", existingUser.google.refresh_token);
     }
 
     await existingUser.save();
